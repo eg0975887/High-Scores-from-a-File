2cd4174efafaee32c47fcfdef5df73e5e5e7e212
eg0975887 <eg0975887@otc.edu>
2025-02-03 00:31:46 -0600

    Added Base Files to Repository
    HighScores.csv holds the information for  the game including each players Initials and Score. Main.java is the main file for parsing and displaying the CSV file. requirments.txt holds the details for what is the purpose and how the project should flow.

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*
+
+      DATE: 01/16/2025
+      ASSIGNMENT: LAB: Module 0 - High Scores
+      VERSION: 0.1.0
+
+ */
+
+
+/////////////////////////////////////////
+//                 Main
+/////////////////////////////////////////
+
+class Main {
+    public static void main(String[] args) { 
+
+    
+      }
+}
